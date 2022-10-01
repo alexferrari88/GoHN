17a617f9c22348cd12db125a2d935c64e37e6843
Alex Ferrari <a.ferrari88@gmail.com>
2022-10-01 08:21:24 +0200

    Add updates wrapper

---
+++ updates.go
@@ -0,0 +1,17 @@
+﻿package gohn
+
+// Update represents a items and profiles changes from the Hacker News API.
+type Update struct {
+	Items    []int    `json:"items"`
+	Profiles []string `json:"profiles"`
+}
+
+// UPDATES_URL is the URL for the updates endpoint.
+const (
+	UPDATES_URL = "https://hacker-news.firebaseio.com/v0/updates.json"
+)
+
+// GetUpdates returns a slice of IDs for the given URL.
+func GetUpdates() (Update, error) {
+	return RetrieveFromURL[Update](UPDATES_URL)
+}
