7c985dca8893d2db5f9332dc5d4f5ed6e93634f0
Alex Ferrari <a.ferrari88@gmail.com>
2022-10-01 08:10:09 +0200

    Implement GetUser

--- users.go
+++ users.go
@@ -1,5 +1,16 @@
 ﻿package gohn
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const (
+	USER_URL = "https://hacker-news.firebaseio.com/v0/user/%s.json"
+)
+
 // User represents a single user from the Hacker News API.
 type User struct {
 	ID        string `json:"id"`
@@ -7,4 +18,28 @@ type User struct {
 	Karma     int    `json:"karma"`
 	About     string `json:"about"`
 	Submitted []int  `json:"submitted"`
+}
+
+// GetUser returns a User given a username.
+func GetUser(username string) (User, error) {
+	var user User
+
+	url := fmt.Sprintf(USER_URL, username)
+	resp, err := http.Get(url)
+	if err != nil {
+		return user, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return user, err
+	}
+
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
 }
