38625b62958ed6964176214c003685e32aaca34c
Alex Ferrari <a.ferrari88@gmail.com>
2022-10-01 17:52:00 +0200

    Add example

---
+++ examples/main.go
@@ -0,0 +1,12 @@
+﻿package main
+
+import "github.com/alexferrari88/gohn"
+
+func main() {
+	// Get the top 10 stories.
+	ids, _ := gohn.GetTopStoriesIDs()
+	for _, id := range ids[:10] {
+		item, _ := gohn.GetItem(id)
+		println(item.Title)
+	}
+}
