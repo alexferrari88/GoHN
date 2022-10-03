a2f8d42689520bc1d32f17845504cdc09046cae0
Alex Ferrari <a.ferrari88@gmail.com>
2022-10-03 06:20:15 +0200

    Add mocking utils

---
+++ test/mocks.go
@@ -0,0 +1,44 @@
+﻿package test
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type MockHTTPClient struct {
+	Responses map[string]*http.Response
+}
+
+func (m *MockHTTPClient) Do(req *http.Request) (*http.Response, error) {
+	if resp, ok := m.Responses[req.URL.String()]; ok {
+		return resp, nil
+	} else {
+		return nil, fmt.Errorf("no response found for request: %v", req)
+	}
+}
+
+func NewMockClient(urls []string, responses []*http.Response) *MockHTTPClient {
+	m := &MockHTTPClient{
+		Responses: make(map[string]*http.Response),
+	}
+
+	for i, url := range urls {
+		m.Responses[url] = responses[i]
+	}
+
+	return m
+}
+
+func NewMockResponse(statusCode int, body any) (*http.Response, error) {
+	b, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+	return &http.Response{
+		StatusCode: statusCode,
+		Body:       io.NopCloser(bytes.NewReader(b)),
+	}, nil
+}
