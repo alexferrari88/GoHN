27f2509dc4100d1543b9da82bc96e135d5a4148a
Alex Ferrari <a.ferrari88@gmail.com>
2022-10-07 10:17:14 +0200

    Fix doc.go example

--- pkg/gohn/doc.go
+++ pkg/gohn/doc.go
@@ -2,70 +2,98 @@
 Package GoHN is a wrapper for the Hacker News API: https://github.com/HackerNews/API
 
 	  Example usage:
-	  	package main
+		package main
 
-	  	import (
-	  		"fmt"
+		import (
+			"context"
+			"fmt"
 
-	  		"github.com/alexferrari88/gohn/pkg/gohn"
-	  		"github.com/alexferrari88/gohn/pkg/processors"
-	  	)
+			"github.com/alexferrari88/gohn/pkg/gohn"
+			"github.com/alexferrari88/gohn/pkg/processors"
+		)
 
-	  	func main() {
-
+		func main() {
 			// Instantiate a new client to retrieve data from the Hacker News API
-			hn := gohn.NewDefaultClient()
+			hn := gohn.NewClient(nil)
+
+			// Use background context
+			ctx := context.Background()
 
 			// Get the top 500 stories' IDs
-			topStoriesIds, _ := hn.GetTopStoriesIDs()
+			topStoriesIds, _ := hn.Stories.GetTopIDs(ctx)
 
+			var story *gohn.Item
 			// Retrieve the details of the first one
-			story, _ := hn.GetItem(topStoriesIds[0])
+			if len(topStoriesIds) > 0 && topStoriesIds[0] != nil {
+				story, _ = hn.Items.Get(ctx, *topStoriesIds[0])
+			}
+
+			if story == nil {
+				panic("No story found")
+			}
 
 			// Print the story's title
-			fmt.Println("Title:", story.Title)
+			fmt.Println("Title:", *story.Title)
 
 			// Print the story's author
-			fmt.Println("Author:", story.By)
+			fmt.Println("Author:", *story.By)
 
 			// Print the story's score
-			fmt.Println("Score:", story.Score)
+			fmt.Println("Score:", *story.Score)
 
 			// Print the story's URL
-			fmt.Println("URL:", story.URL)
+			fmt.Println("URL:", *story.URL)
 
 			fmt.Println()
 			fmt.Println()
+
+			if story.Kids == nil {
+				fmt.Println("No comments found")
+				return
+			}
 
 			// Retrieve all the comments for that story
 			// UnescapeHTML is applied to each retrieved item to unescape HTML characters
-			commentsMap := hn.RetrieveKidsItems(story, processors.UnescapeHTML())
+
+			commentsMap, err := hn.Items.FetchAllDescendants(ctx, story, processors.UnescapeHTML())
+
+			if err != nil {
+				panic(err)
+			}
+			if len(commentsMap) == 0 {
+				fmt.Println("No comments found")
+				return
+			}
 
 			fmt.Printf("Comments found: %d\n", len(commentsMap))
 			fmt.Println()
 
 			// Create a Story struct to hold the story and its comments
-
-				storyWithComments := gohn.Story{
-					StoryItem:       story,
-					CommentsByIdMap: commentsMap,
-				}
+			storyWithComments := gohn.Story{
+				Parent:          story,
+				CommentsByIdMap: commentsMap,
+			}
 
 			// Calculate the position of each comment in the story
 			storyWithComments.SetCommentsPosition()
 
 			// Get an ordered list of comments' IDs (ordered by position)
-			orderedIDs := storyWithComments.GetOrderedCommentsIDs()
+			orderedIDs, err := storyWithComments.GetOrderedCommentsIDs()
+
+			if err != nil {
+				panic(err)
+			}
 
 			// Print the comments
-
-				for _, id := range orderedIDs {
-					comment := commentsMap[id]
-					fmt.Println(comment.Text)
+			for _, id := range orderedIDs {
+				comment := commentsMap[id]
+				if comment.Text != nil {
+					fmt.Println(*comment.Text)
 					fmt.Println()
 				}
+			}
 
-}
+		}
 */
 package gohn
 
